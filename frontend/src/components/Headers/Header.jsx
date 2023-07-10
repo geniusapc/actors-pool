bf0bb0c683de4268633d237bc1d7ee9b3514f197
Prince Arthur <geniusapc@gmail.com>
2023-07-10 12:14:28 +0100

    feat: docker compose

--- frontend/src/components/Headers/Header.jsx
+++ frontend/src/components/Headers/Header.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import InputWithButton from '../Input/InputWithButton';
-import { ReactComponent as HamBugerIcon } from "../../assets/icons/hamburger.svg"
-import { ReactComponent as Logo } from "../../assets/icons/logo.svg"
+import { ReactComponent as HamBugerIcon } from '../../assets/icons/hamburger.svg';
+import { ReactComponent as Logo } from '../../assets/icons/logo.svg';
 import AuthUser from './AuthUser';
 import { openSignUpModal } from '../../features/auth/auth';
 import { useDispatch, useSelector } from 'react-redux';
@@ -22,8 +22,7 @@ function Header({ showTalentHidden = false }) {
 
     const onSubmitTalentHandler = (e) => {
         e.preventDefault();
-        if (talentSearchValue)
-            navigate(`/talents?q=${talentSearchValue}`);
+        if (talentSearchValue) navigate(`/talents?q=${talentSearchValue}`);
         else navigate(`/talents`);
     };
 
@@ -33,9 +32,7 @@ function Header({ showTalentHidden = false }) {
                 <div className="flex items-center justify-between">
                     <div className="flex items-center justify-start">
                         <button
-                            data-drawer-target="logo-sidebar"
-                            data-drawer-toggle="logo-sidebar"
-                            aria-controls="logo-sidebar"
+                            data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar"
                             type="button"
                             className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                         >
@@ -53,7 +50,7 @@ function Header({ showTalentHidden = false }) {
                                     <InputWithButton
                                         style={{ input: 'h-10' }}
                                         placeHolder="search for talent"
-                                        value={talentSearchValue || ""}
+                                        value={talentSearchValue || ''}
                                         hideBtn
                                         onSubmit={onSubmitTalentHandler}
                                         onChange={onChangeSearchTalent}
@@ -71,6 +68,34 @@ function Header({ showTalentHidden = false }) {
                                 </button>
                             )}
                         </div>
+                    </div>
+
+                    <div
+                        className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+                        id="logo-sidebar"
+                    >
+                        <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:bg-transparent md:flex-row md:space-x-8 md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                            <li>
+                                <Link
+                                    to="/"
+                                    className="block py-2 pl-3 pr-4 text-white  md:text-white  bg-blue-700 rounded md:bg-transparent  md:p-0 md:dark:text-blue-500"
+                                    aria-current="page"
+                                >
+                                    Home
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    to="/about"
+                                    className="block py-2 pl-3 pr-4 text-gray-900  md:text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:p-0 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                                >
+                                    About
+                                </Link>
+                            </li>
+                            <li>
+                                <button className="text-gray-900  md:text-white pl-3 pr-4">Login</button>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
--- frontend/src/components/SideBar/SideBar.jsx
+++ frontend/src/components/SideBar/SideBar.jsx
@@ -48,7 +48,7 @@ function SideBar({ role = "User" }) {
 
     return (
         <aside
-            id="logo-sidebar"
+            id="default-sidebar"
             className="fixed top-0 left-0 z-40 w-64 md:w-24 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
             aria-label="Sidebar"
         >
